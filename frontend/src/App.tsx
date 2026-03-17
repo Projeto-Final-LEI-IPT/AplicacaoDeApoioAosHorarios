6d98ad4371744f780257439127940eace1afb5de
RicMFM <101366786+RicMFM@users.noreply.github.com>
2026-03-17 02:22:28 +0000

    feat: página 404 criada closes #9

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage.tsx'
+import MainLayout from './Layout/MainLayout.tsx'
+import PageNotFound from './pages/PageNotFound.tsx'
+import HorarioPage from './pages/HorarioPage.tsx'
 
 function App() {
   return (
@@ -7,6 +10,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
+
+        {/*Páginas com navbar*/}
+        <Route path="/horario" element={<MainLayout />}>
+          <Route index element={<HorarioPage />} />
+        </Route>
+
+        {/* Página 404 */}
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/PageNotFound.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function NotFoundPage() {
+  const navigate = useNavigate()
+  const [hover, setHover] = useState(false)
+
+  return (
+    <div style={{
+      minHeight: '100vh',
+      background: '#fff',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: '#000000',
+    }}>
+      <div style={{ fontSize: '6rem', fontWeight: 800, color: 'rgb(4, 4, 4)' }}>404</div>
+      <h1 style={{ fontSize: '1.5rem', fontWeight: 700, marginBottom: '8px' }}>
+        Página não encontrada
+      </h1>
+      <p style={{ color: 'rgba(0, 0, 0, 0.5)', marginBottom: '32px' }}>
+        A página que procurada não existe.
+      </p>
+      <button
+        onClick={() => navigate('/horario')}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        style={{
+          background: hover ? '#1f600ccc' : '#66aa22cc',
+          border: 'none',
+          borderRadius: '8px',
+          padding: '12px 24px',
+          color: '#fff',
+          fontWeight: 700,
+          cursor: 'pointer',
+        }}
+      >
+        Voltar à página de Horários
+      </button>
+    </div>
+  )
+}
