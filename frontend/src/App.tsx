4af8459a4a166c9708546d20a72149a40410eec3
RicMFM <101366786+RicMFM@users.noreply.github.com>
2026-04-22 01:51:00 +0100

    feat: rotas das páginas de gestão adicionadas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,11 @@ import LoginPage from './pages/LoginPage.tsx'
 import MainLayout from './Layout/MainLayout.tsx'
 import PageNotFound from './pages/PageNotFound.tsx'
 import HorarioPage from './pages/HorarioPage.tsx'
+import SalasPage from './pages/SalasPage.tsx'
+import ImportacaoPage from './pages/ImportacaoPage.tsx'
+import DocentesPage from './pages/DocentesPage.tsx'
+import UcsPage from './pages/UcsPage.tsx'
+import TurmasPage from './pages/TurmasPage.tsx'
 
 function App() {
   return (
@@ -14,13 +19,18 @@ function App() {
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
 
-          {/*Páginas com navbar*/}
-          <Route path="/horario" element={
+          {/* Páginas com navbar */}
+          <Route path="/" element={
             <PrivateRoute>
               <MainLayout />
             </PrivateRoute>
           }>
-            <Route index element={<HorarioPage />} />
+            <Route path="horario" element={<HorarioPage />} />
+            <Route path="salas" element={<SalasPage />} />
+            <Route path="importar" element={<ImportacaoPage />} />
+            <Route path="docentes" element={<DocentesPage />} />
+            <Route path="cursos" element={<UcsPage />} />
+            <Route path="turmas" element={<TurmasPage />} />
           </Route>
 
           {/* Página 404 */}
